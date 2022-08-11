7274408fd595be4de9945f65af6c2dfe6159953d
praveenavaithi <praveenavaithi@gmail.com>
2022-08-11 20:16:56 +0530

    Commmited for maven yml files

--- src/main/java/com/poc/cicdpipeline/SpringBootCICDPipeline/SpringBootCicdPipelineApplication.java
+++ src/main/java/com/poc/cicdpipeline/SpringBootCICDPipeline/SpringBootCicdPipelineApplication.java
@@ -2,8 +2,9 @@ package com.poc.cicdpipeline.SpringBootCICDPipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringBootCicdPipelineApplication {
 
 	public static void main(String[] args) {
