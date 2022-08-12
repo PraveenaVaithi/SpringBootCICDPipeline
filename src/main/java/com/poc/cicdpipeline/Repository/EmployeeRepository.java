52bfbe37084a1668d57cf40695e9657e3c1592e2
praveenavaithi <praveenavaithi@gmail.com>
2022-08-12 12:52:08 +0530

    Commmited for normal spring boot appplication

--- src/main/java/com/poc/cicdpipeline/Repository/EmployeeRepository.java
+++ src/main/java/com/poc/cicdpipeline/Repository/EmployeeRepository.java
@@ -1,9 +1,9 @@
-//package com.poc.cicdpipeline.Repository;
-//
-//import com.poc.cicdpipeline.Entity.PermanentEmployee;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface EmployeeRepository extends CrudRepository<PermanentEmployee, Integer> {
-//}
+package com.poc.cicdpipeline.Repository;
+
+import com.poc.cicdpipeline.Entity.PermanentEmployee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<PermanentEmployee, Integer> {
+}
--- src/main/java/com/poc/cicdpipeline/Service/EmployeeServiceImpl.java
+++ src/main/java/com/poc/cicdpipeline/Service/EmployeeServiceImpl.java
@@ -1,17 +1,18 @@
-//package com.poc.cicdpipeline.Service;
-//
-//import com.poc.cicdpipeline.Entity.PermanentEmployee;
-//import com.poc.cicdpipeline.Repository.EmployeeRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class EmployeeServiceImpl implements EmployeeService{
-//
-//    @Autowired
-//    EmployeeRepository employeeRepository;
-//    @Override
-//    public PermanentEmployee insertEmployeeDetails(PermanentEmployee permanentEmployee) {
-//        return employeeRepository.save(permanentEmployee);
-//    }
-//}
+package com.poc.cicdpipeline.Service;
+
+import com.poc.cicdpipeline.Entity.PermanentEmployee;
+import com.poc.cicdpipeline.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Override
+    public PermanentEmployee insertEmployeeDetails(PermanentEmployee permanentEmployee) {
+       System.out.println("Enter");
+        return employeeRepository.save(permanentEmployee);
+    }
+}
