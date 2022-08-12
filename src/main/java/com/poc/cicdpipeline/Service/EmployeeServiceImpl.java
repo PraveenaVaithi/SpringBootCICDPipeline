1fcfe1fe30239a091a31a180dbb3c4063208f1d3
praveenavaithi <praveenavaithi@gmail.com>
2022-08-12 16:36:10 +0530

    Commmited for normal spring boot appplication

--- src/main/java/com/poc/cicdpipeline/Service/EmployeeServiceImpl.java
+++ src/main/java/com/poc/cicdpipeline/Service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.poc.cicdpipeline.Service;
 import com.poc.cicdpipeline.Entity.PermanentEmployee;
 import com.poc.cicdpipeline.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +13,7 @@ public class EmployeeServiceImpl implements EmployeeService{
     EmployeeRepository employeeRepository;
     @Override
     public PermanentEmployee insertEmployeeDetails(PermanentEmployee permanentEmployee) {
-       System.out.println("Enter");
+//       System.out.println("Enter");
         return employeeRepository.save(permanentEmployee);
     }
 }
